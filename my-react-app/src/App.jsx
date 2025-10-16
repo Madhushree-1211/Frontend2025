3de30d6215bbd68513cf672459666973afbc9408
Madhushree-1211 <mpd98340@gmail.com>
2025-10-16 10:32:32 +0530

    Add files via upload
    my-react-app

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,14 @@
+import Header from "./components/Header";
+
+const App = () =>{
+  return (
+    <>
+    <Header />{" "}
+    <div>
+      <h1>Hello From React</h1>
+      <p>I am Madhushree,from AIML</p>
+    </div>
+    </>
+  );
+}
+export default App;
